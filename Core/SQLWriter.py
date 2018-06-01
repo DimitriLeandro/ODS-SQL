0ce43f47af886ff4be930cf1383184c97122533c
Dimitri Leandro <dimitri.leandro@gmail.com>
2018-06-01 04:37:31 -0300

    Passando para orientação a objetos

---
+++ Core/SQLWriter.py
@@ -0,0 +1,78 @@
+class SQLWriter():
+
+
+    def __init__(self):
+        self.sqlCreateDatabase = ""
+        self.sqlCreateTable = ""
+        self.sqlInsert = ""
+
+
+    def askDatabaseName(self):
+        dbName = input("Enter database name: ")
+        self.sqlCreateTable += "CREATE DATABASE " + dbName + ";\n"
+        self.sqlCreateTable += "USE " + dbName + ";\n\n"
+
+    def getTablesName(self, objJson):
+        arrayTables = []
+        for table in objJson:
+            arrayTables.append(table)
+        return arrayTables
+
+    def setCreateTable(self, table):
+        self.sqlCreateTable += "CREATE TABLE " + table + " (\n"
+        self.sqlCreateTable += "\tid INT NOT NULL AUTO_INCREMENT,\n"
+
+    def closeTableScript(self, table):
+        self.sqlCreateTable += "\tCONSTRAINT pk_" + table + " PRIMARY KEY (id)\n"
+        self.sqlCreateTable += ");\n\n"
+
+    def getTableColumns(self, objJson, table):
+        arrayColumns = []
+        for field in objJson[table][0]:
+            arrayColumns.append(field)
+        return arrayColumns
+
+    def askColumnType(self, column, length):
+        type = input(column + " -> ")
+        type = type.upper()
+        self.sqlCreateTable += "\t" + column + " " + type
+        self.sqlCreateTable += ",\n"
+
+    def startInsert(self, table, arrayColumns):
+        self.sqlInsert += "INSERT INTO " + table + " ("
+        p = 0
+        for column in arrayColumns:
+            p += 1
+            if p == len(arrayColumns):
+                self.sqlInsert += column
+            else:
+                self.sqlInsert += column + ", "
+        self.sqlInsert += ") VALUES\n"
+
+    def insertValues(self, objJson, table, arrayColumns):
+        q = 0
+        for row in objJson[table]:
+            q += 1
+            if q != 1:
+                self.sqlInsert += "("
+                j = 0
+                for value in row:
+                    j += 1
+                    if (j != len(arrayColumns)):
+                        self.sqlInsert += "'" + str(value) + "', "
+                    else:
+                        self.sqlInsert += "'" + str(value) + "'"
+                if j != len(arrayColumns):
+                    qtLeft = len(arrayColumns) - j
+                    for k in range(0, qtLeft):
+                        if k == qtLeft - 1:
+                            self.sqlInsert += "''"
+                        else:
+                            self.sqlInsert += "'',"
+                if q == len(objJson[table]):
+                    self.sqlInsert += ");\n\n"
+                else:
+                    self.sqlInsert += "),\n"
+
+    def getFullSQLScript(self):
+        return (self.sqlCreateDatabase + self.sqlCreateTable + self.sqlInsert)
--- Core/__init__.py
+++ Core/__init__.py
@@ -1,10 +1,6 @@
 from pyexcel_ods import get_data
+from SQLWriter import SQLWriter
 import json
-
-
-sqlCreateDatabase = ""
-sqlCreateTable = ""
-sqlInsert = ""
 
 
 def openOdsFile():
@@ -16,108 +12,27 @@ def openOdsFile():
             print("File not found. Please, try again.")
 
 
-def askDatabaseName():
-    dbName = input("Enter database name: ")
-    global sqlCreateTable
-    sqlCreateTable += "CREATE DATABASE " + dbName + ";\n"
-    sqlCreateTable += "USE " + dbName + ";\n\n"
-
-
-def getTablesName(objJson):
-    arrayTables = []
-    for table in objJson:
-        arrayTables.append(table)
-    return arrayTables
-
-
-def setCreateTable(table):
-    global sqlCreateTable
-    sqlCreateTable += "CREATE TABLE " + table + " (\n"
-    sqlCreateTable += "\tid INT NOT NULL AUTO_INCREMENT,\n"
-
-
-def closeTableScript(table):
-    global sqlCreateTable
-    sqlCreateTable += "\tCONSTRAINT pk_" + table + " PRIMARY KEY (id)\n"
-    sqlCreateTable += ");\n\n"
-
-
-def getTableColumns(objJson, table):
-    arrayColumns = []
-    for field in objJson[table][0]:
-        arrayColumns.append(field)
-    return arrayColumns
-
-
-def askColumnType(column, i, length):
-    type = input(column + " -> ")
-    type = type.upper()
-    global sqlCreateTable
-    sqlCreateTable += "\t" + column + " " + type
-    if i == length:
-        sqlCreateTable += "\n"
-    else:
-        sqlCreateTable += ",\n"
-
-
-def startInsert(table, arrayColumns):
-    global sqlInsert
-    sqlInsert += "INSERT INTO " + table + " ("
-    p = 0
-    for column in arrayColumns:
-        p += 1
-        if p == len(arrayColumns):
-            sqlInsert += column
-        else:
-            sqlInsert += column + ", "
-    sqlInsert += ") VALUES\n"
-
-
-def insertValues(objJson, table, arrayColumns):
-    global sqlInsert
-    q = 0
-    for row in objJson[table]:
-        q += 1
-        if q != 1:
-            sqlInsert += "("
-            j = 0
-            for value in row:
-                j += 1
-                if (j != len(arrayColumns)):
-                    sqlInsert += "'" + str(value) + "', "
-                else:
-                    sqlInsert += "'" + str(value) + "'"
-            if j != len(arrayColumns):
-                qtLeft = len(arrayColumns) - j
-                for k in range(0, qtLeft):
-                    if k == qtLeft - 1:
-                        sqlInsert += "''"
-                    else:
-                        sqlInsert += "'',"
-            if q == len(objJson[table]):
-                sqlInsert += ");\n\n"
-            else:
-                sqlInsert += "),\n"
-
-
 # ------STARTING-------
 objJson = openOdsFile()
-askDatabaseName()
-arrayTables = getTablesName(objJson)
+objSQLWriter = SQLWriter()
+objSQLWriter.askDatabaseName()
+arrayTables = objSQLWriter.getTablesName(objJson)
 # ---FOR EACH TABLE----
 for table in arrayTables:
     # -------------------------CREATE TABLE SCRIPT-----------------------------
-    setCreateTable(table)
-    arrayColumns = getTableColumns(objJson, table)
+    objSQLWriter.setCreateTable(table)
+    arrayColumns = objSQLWriter.getTableColumns(objJson, table)
     print("Set columns type for table " + table + ": (Exemple: VARCHAR(30))")
     i = 0
     for column in arrayColumns:
         i += 1
-        askColumnType(column, i, len(arrayColumns))
-    closeTableScript(table)
+        objSQLWriter.askColumnType(column, len(arrayColumns))
+    objSQLWriter.closeTableScript(table)
     # -----------------------INSERT VALUES INTO TABLE--------------------------
-    startInsert(table, arrayColumns)
-    insertValues(objJson, table, arrayColumns)
+    objSQLWriter.startInsert(table, arrayColumns)
+    objSQLWriter.insertValues(objJson, table, arrayColumns)
 # ------FINISHING-------
-sqlFullScript = sqlCreateDatabase + sqlCreateTable + sqlInsert
-print(sqlFullScript)
+objFile = open('sql.txt','w')
+objFile.write(objSQLWriter.getFullSQLScript())
+objFile.close()
+print(objSQLWriter.getFullSQLScript())
