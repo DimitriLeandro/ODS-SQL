aa427646cab4e112e03ad66be692ce400d8d0c3a
Dimitri Leandro <dimitri.leandro@gmail.com>
2018-06-03 02:48:48 -0300

    Aparentemente tudo funcionando

--- Core/FileHandler.py
+++ Core/FileHandler.py
@@ -6,28 +6,29 @@ import getpass
 
 class FileHandler():
 
-    def openOdsFile(self):
-        while True:
-            fileName = input("Enter path and ODS filename (Exemple: /home/" + getpass.getuser() + "/Documents/sheet.ods): ")
-            try:
-                return json.loads(json.dumps(get_data("../../.." + fileName)))
-            except:
-                print("File not found. Please, try again.")
+	def openOdsFile(self):
+		while True:
+			fileName = input("Enter path and ODS filename (Exemple: /home/" + getpass.getuser() + "/Documents/sheet.ods): ")
+			try:
+				return json.loads(json.dumps(get_data("../.." + fileName)))
+			except:
+				print("File not found. Please, try again.")
 
 
-    def verifyCurrentDirectory(self):
-        if os.getcwd() != "/opt/ODS-SQL/Core":
-            print("You must copy ODS-SQL folder to /opt/")
-            return False
-        else:
-            return True
+	def verifyCurrentDirectory(self):
+		if os.getcwd() != "/home/" + getpass.getuser() :
+			print("You must copy 'ODS-SQL' folder to '/opt/' and run '$ python3 /opt/ODS-SQL/Core/' in terminal from '/home/user/'")
+			print("Current path: " + os.getcwd())
+			return False
+		else:
+			return True
 
 
-    def createSQLFile(self, script, dbName):
-        try:
-            objFile = open("../../../home/" + getpass.getuser() + "/" + dbName + ".sql", 'w')
-            objFile.write(script)
-            print("Document saved in: /home/" + getpass.getuser() + "/" + dbName + ".sql")
-            objFile.close()
-        except:
-            print("Error trying to create /home/" + getpass.getuser() + "/" + dbName + ".sql")
+	def createSQLFile(self, script, dbName):
+		try:
+			objFile = open("../../home/" + getpass.getuser() + "/" + dbName + ".sql", 'w')
+			objFile.write(script)
+			print("Document saved in: /home/" + getpass.getuser() + "/" + dbName + ".sql")
+			objFile.close()
+		except:
+			print("Error trying to create /home/" + getpass.getuser() + "/" + dbName + ".sql")
